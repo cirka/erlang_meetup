fec1c1be67bfe5e1a14939c469db81bc85961aa5
Ricardas Pocius <cirka@cirka.lt>
2016-05-10 18:50:08 +0300

    Meetup #16: udp echo server

---
+++ meetup_16/udp/src/udp_app.erl
@@ -0,0 +1,16 @@
+-module(udp_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    udp_sup:start_link().
+
+stop(_State) ->
+    ok.
