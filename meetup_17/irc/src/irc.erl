fd9cdefb1a082965966c5cce7630aafdbcecbfd6
Ričardas Pocius <cirka@cirka.lt>
2016-06-08 16:18:15 +0300

    Add/fix Spec, fix session stoping

--- meetup_17/irc/src/irc.erl
+++ meetup_17/irc/src/irc.erl
@@ -3,7 +3,7 @@
 -compile(export_all).
 
 
--spec start_session( SessionName :: term(),
+-spec start_session(SessionName :: term(),
                     Server :: list(),
                     Nick :: list(),
                     Username :: list(),
@@ -15,11 +15,16 @@ start_session(SessionName, Server, Nick, Username, RealName) ->
 
 -spec stop_session(SessionName :: term()) -> ok.
 stop_session(SessionName) ->
-    irc_clients_sup:stop_client(SessionName).
+    irc_sessions_sup:stop_session(SessionName).
 
 -spec send_msg(Session :: term(), Msg :: term()) -> ok | {error, Reason :: term()}.
 send_msg(Session,Msg) ->
     irc_client:send_msg(Session,Msg).
+
+-spec add_handler(Session :: term(),
+                  Handler :: fun((Event :: term, State :: term()) -> {ok,NewState :: term()}),
+                  InitialState :: term()) ->
+     NewState :: term().
 
 add_handler(Session,Handler,InitialState) ->
     irc_event_server:add_handler(Session,Handler,InitialState).
